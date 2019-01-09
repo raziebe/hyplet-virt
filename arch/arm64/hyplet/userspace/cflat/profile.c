13d1e602562eaf1f6e9d6a2d67520096b2d3205c
raz <raziebe@gmail.com>
2019-01-09 22:27:24 +0200

    local rpc works

--- arch/arm64/hyplet/userspace/cflat/profile.c
+++ arch/arm64/hyplet/userspace/cflat/profile.c
@@ -11,13 +11,16 @@
 
 #define NR_OPCODES	100
 
+int func_id = 8;
 
 __attribute__ ((section("hyp_rw")))  unsigned int cnt = 0;
 __attribute__ ((section("hyp_rw")))  unsigned long instr[NR_OPCODES][3] = {0};
 
 __attribute__ ((section("hyp_rx")))  long record_opcode(long fault_addr,long a,long b)
 {
-	instr[cnt++][0] = fault_addr;
+	instr[cnt][0] = fault_addr;
+	instr[cnt][1] = a;
+	instr[cnt++][2] = b;
 	return 0;
 }
 
@@ -33,17 +36,22 @@ static int hyplet_start(int argc, char *argv[])
 {
 	int rc;
 	int cpu = -1;
-	int stack_size = sysconf(_SC_PAGESIZE) * 1;
+	int stack_size = sysconf(_SC_PAGESIZE) * 2;
 	void *stack_addr;
+
 	long hyp_sec_x;
 	int  hyp_sec_x_size;
+
 	long hyp_sec_rw;
 	int  hyp_sec_rw_size;
 
-    	if (Elf_parser_load_memory_map(argv[0])){
+	long hyp_sec_rx;
+	int hyp_sec_rx_size;
+
+  	if (Elf_parser_load_memory_map(argv[0])){
 		printf("Failed to ELF parser\n");
 		return -1;
-    	}
+  	}
 
 	if (!get_section_addr("hyp_x", &hyp_sec_x, &hyp_sec_x_size)) {
 		fprintf(stderr, "hyp_x: \n");
@@ -54,6 +62,12 @@ static int hyplet_start(int argc, char *argv[])
 		fprintf(stderr, "hyp_rw:\n");
 		return -1;
 	}	
+
+	if (!get_section_addr("hyp_rx", &hyp_sec_rx, &hyp_sec_rx_size)) {
+		fprintf(stderr, "hyp_rw:\n");
+		return -1;
+	}
+
 	
 	if (hyplet_map_vma((void *)hyp_sec_rw, cpu)) {
 		fprintf(stderr, "hyplet: Failed to hyp_rw\n");
@@ -66,6 +80,11 @@ static int hyplet_start(int argc, char *argv[])
 		return -1;
 	}
 
+	if (hyplet_map_vma((void*)hyp_sec_rx, cpu)) {
+		fprintf(stderr, "hyplet: Failed to map sec_x\n");
+		return -1;
+	}
+
 	rc = posix_memalign(&stack_addr,
 			    sysconf(_SC_PAGESIZE), stack_size);
 	if (rc < 0) {
@@ -73,17 +92,11 @@ static int hyplet_start(int argc, char *argv[])
 		return -1;
 	}
 //
-// must fault it
+// must fault the stack
 //
 	memset(stack_addr, 0x00, stack_size);
-
-	if (hyplet_map(record_opcode, 1, cpu)) {
-		fprintf(stderr, "hyplet: Failed to map test opcode\n");
-		return -1;
-	}
-
-	fprintf(stderr, "hyplet: stack addr %x\n", stack_addr);
-	if (hyplet_map(stack_addr, stack_size, cpu)) {
+	printf("Setting stack at %p\n", stack_addr);
+	if (hyplet_set_stack(stack_addr, stack_size, cpu)) {
 		fprintf(stderr, "hyplet: Failed to map a stack\n");
 		return -1;
 	}
@@ -93,15 +106,15 @@ static int hyplet_start(int argc, char *argv[])
 		return -1;
 	}
 
-        if (hyplet_set_rpc(record_opcode, cpu)) {
-             fprintf(stderr, "hyplet: Failed to assign opcode\n");
-             return -1;
-        }
+	if (hyplet_set_callback(record_opcode, cpu)) {
+		fprintf(stderr, "hyplet: Failed to map test opcode\n");
+		return -1;
+	}
 
-        if (hyplet_mdcr_on(cpu)){
-                fprintf(stderr, "hyplet: set trap failed\n");
-                return -1;
-        }
+    if (hyplet_rpc_set(record_opcode, func_id, cpu)) {
+         fprintf(stderr, "hyplet: Failed to assign opcode\n");
+         return -1;
+    }
 
 	fprintf(stderr, "hyplet: set mdcr on XXXXXXX\n");
 	return 0;
@@ -119,7 +132,8 @@ int main(int argc, char *argv[])
     if (hyplet_start(argc, argv)) {
 	return -1;
     }
+
     printf("\nHyplet set. Prepare to run trap\n");
-    asm volatile("brk #5\n" : );
-    sleep(1);
+    hyplet_rpc_call(func_id, 3, 4, 5);
+    printf("Got %lx,%lx,%lx\n", instr[0][0],   instr[0][1],  instr[0][2] );
 }
