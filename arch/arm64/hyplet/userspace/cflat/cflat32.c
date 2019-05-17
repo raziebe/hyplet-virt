e4c0d0582b24837c5ca14e3bc9e4ce3e830bf009
raz <raziebe@gmail.com>
2019-05-17 19:28:07 +0300

    bug fix. now the user can access 4 arguments

---
+++ arch/arm64/hyplet/userspace/cflat/cflat32.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+
+void hyplet_rpc_call(int funcid,int a1,int a2,int a3,int a4)
+{
+    __asm("mov r0,%0\n"
+		: 
+		:"r" (funcid)
+		);
+    __asm("mov r1,%0\n"
+		: 
+		:"r" (a1)
+		);
+    __asm("mov r2,%0\n"
+		: 
+		:"r" (a2)
+		);
+    __asm("mov r3,%0\n"
+		: 
+		:"r" (a3)
+		);
+    __asm("mov r4,%0\n"
+		: 
+		:"r" (a4)
+		);
+    __asm("bkpt #3\n":::);
+}
