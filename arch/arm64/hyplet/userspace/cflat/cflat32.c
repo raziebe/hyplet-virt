6016839c41f8ed7466ef8c9b648450c36a046fcf
raz <raziebe@gmail.com>
2019-05-17 19:29:23 +0300

    32 bit test units

--- arch/arm64/hyplet/userspace/cflat/cflat32.c
+++ arch/arm64/hyplet/userspace/cflat/cflat32.c
@@ -5,25 +5,21 @@
 #include <string.h>
 
 
-void hyplet_rpc_call(int funcid,int a1,int a2,int a3,int a4)
+void hyplet_rpc_call(int a1,int a2,int a3,int a4)
 {
     __asm("mov r0,%0\n"
 		: 
-		:"r" (funcid)
+		:"r" (a1)
 		);
     __asm("mov r1,%0\n"
 		: 
-		:"r" (a1)
+		:"r" (a2)
 		);
     __asm("mov r2,%0\n"
 		: 
-		:"r" (a2)
-		);
-    __asm("mov r3,%0\n"
-		: 
 		:"r" (a3)
 		);
-    __asm("mov r4,%0\n"
+    __asm("mov r3,%0\n"
 		: 
 		:"r" (a4)
 		);
--- arch/arm64/hyplet/userspace/cflat/test_cflat32.c
+++ arch/arm64/hyplet/userspace/cflat/test_cflat32.c
@@ -4,34 +4,7 @@
 #include <errno.h>
 #include <string.h>
 
-
-int func_id = 8;
-
-static inline  void hyplet_rpc_call(int funcid,int a1,int a2,int a3,int a4)
-{
-    __asm("mov r0,%0\n"
-		: 
-		:"r" (funcid)
-		);
-    __asm("mov r1,%0\n"
-		: 
-		:"r" (a1)
-		);
-    __asm("mov r2,%0\n"
-		: 
-		:"r" (a2)
-		);
-    __asm("mov r3,%0\n"
-		: 
-		:"r" (a3)
-		);
-    __asm("mov r4,%0\n"
-		: 
-		:"r" (a4)
-		);
-    __asm("bkpt #3\n":::);
-}
-
+void hyplet_rpc_call(int a1,int a2,int a3,int a4);
 
 /*
  * it is essential affine the program to the same 
@@ -39,5 +12,5 @@ static inline  void hyplet_rpc_call(int funcid,int a1,int a2,int a3,int a4)
 */
 int main(int argc, char *argv[])
 {
-    hyplet_rpc_call(func_id, 6, 7, 9,111);
+    hyplet_rpc_call(6, 7, 9, 0x111);
 }
